b71c2782844cecae212d5ea06fdafc604486344e
letbrasil <le.brasileiro@gmail.com>
2020-11-06 20:15:25 -0300

    estilização, revisão de variáveis e inclusão if símbolos

--- src/cipher.js
+++ src/cipher.js
@@ -1,23 +1,52 @@
 const cipher = {
-  /* 
-  encode: function(offset, yourMsg) {
-    
-  },
-
-  decode: function(offset, yourMsg) {
-    
-  }
-
-            OU
 
   encode(offset, yourMsg) {
-
-  }
+    const abcLength = 26; // tamanho do alfabeto
+    const asciiLetterA = 65; // correspondente à primeira letra do alfabeto MAIÚSCULO no ASCII
+    let finalMsg = ""; // variável que armazenará msg codificada
+    
+    if (!offset || !yourMsg) {
+      throw new TypeError("ERRO: Campos não preenchidos ou deslocamento 0(zero)");
+    } else {
+      for (let p = 0; p < yourMsg.length; p++) {
+        const initialAscii = yourMsg.charCodeAt(p); // código ASCII da letra inserida sem deslocamento
+        if (initialAscii >= 65 && initialAscii <=90) {
+          const newAscii = (((initialAscii + asciiLetterA + offset) % abcLength) + asciiLetterA); // código ASCII da letra após deslocamento
+          const showLetter = String.fromCharCode(newAscii);
+          finalMsg += showLetter;
+        } else {
+          const newAscii = initialAscii;
+          const showLetter = String.fromCharCode(newAscii);
+          finalMsg += showLetter;
+        }
+      }
+      return finalMsg;
+    }
+  },
 
   decode(offset, yourMsg) {
-
+    const abcLength = 26; // tamanho do alfabeto
+    const asciiLetterA = 65; // correspondente à primeira letra do alfabeto no ASCII
+    let finalMsg = ""; // variável que armazenará msg decodificada
+    
+    if (!offset || !yourMsg) {
+      throw new TypeError("ERRO: Campos não preenchidos ou deslocamento 0(zero)");
+    } else {
+      for (let p = 0; p < yourMsg.length; p++) {
+        const initialAscii = yourMsg.charCodeAt(p); // código ASCII da letra inserida sem deslocamento
+        if (initialAscii >= 65 && initialAscii <=90) {
+          const newAscii = (((initialAscii + asciiLetterA - offset) % abcLength) + asciiLetterA); // código ASCII da letra após deslocamento
+          const showLetter = String.fromCharCode(newAscii);
+          finalMsg += showLetter;
+        } else {
+          const newAscii = initialAscii;
+          const showLetter = String.fromCharCode(newAscii);
+          finalMsg += showLetter;
+        }
+      }
+      return finalMsg;
+    }
   }
-  */
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,14 @@
 import cipher from './cipher.js';
 
 
-console.log(cipher);
-
-
-// Declarar variáveis para alfabeto e ASCII
-
-const abcLength = 26; // tamanho do alfabeto
-const asc1stLetter = 65; // correspondente à primeira letra do alfabeto no ASCII
-
-
 const btnEncoding = document.getElementById("btn-enc"); // variável botão CODIFICAR
 btnEncoding.addEventListener("click", encrypt);
 
 function encrypt() {
     const yourMsg = document.getElementById("msg").value.toUpperCase(); // msg digitada
     const offset = Number(document.getElementById("offset-key").value); // chave de deslocamento
-    let finalMsg = ""; // variável que armazenará msg codificada
-        for (let p = 0; p < yourMsg.length; p++) {
-            let initialAsc = yourMsg.charCodeAt(p); // código ASCII da letra inserida sem deslocamento
-            let newAsc = (((initialAsc + asc1stLetter + offset) % abcLength) + asc1stLetter); // código ASCII da letra após deslocamento
-            let showLetter = String.fromCharCode(newAsc);
-            finalMsg += showLetter;
-        }
-        
-        document.getElementById("final-msg").innerHTML = "Sua mensagem <strong>codificada</strong> é: <br><br>" + finalMsg;
+    const finalMsg = cipher.encode(offset, yourMsg);
+    document.getElementById("final-msg").innerHTML = "Sua mensagem <strong>codificada</strong> é: <br><br>" + finalMsg;
 }
 
 
@@ -34,15 +18,8 @@ btnDecoding.addEventListener("click", decrypt);
 function decrypt() {
     const yourMsg = document.getElementById("msg").value.toUpperCase(); // msg digitada
     const offset = Number(document.getElementById("offset-key").value); // chave de deslocamento
-    let finalMsg = ""; // variável que armazenará msg decodificada
-        for (let p = 0; p < yourMsg.length; p++) {
-            let initialAsc = yourMsg.charCodeAt(p); // código ASCII da letra inserida sem deslocamento
-            let newAsc = (((initialAsc + asc1stLetter - offset) % abcLength) + asc1stLetter); // código ASCII da letra após deslocamento
-            let showLetter = String.fromCharCode(newAsc);
-            finalMsg += showLetter;
-        }
-        
-        document.getElementById("final-msg").innerHTML = "Sua mensagem <strong>decodificada</strong> é: <br><br>" + finalMsg;
+    const finalMsg = cipher.decode(offset, yourMsg);
+    document.getElementById("final-msg").innerHTML = "Sua mensagem <strong>decodificada</strong> é: <br><br>" + finalMsg;
 }
 
 
